73d73600c24ac745d732532d1fb63cdd9cd106d1
Babak Kamali <b.kamali@yarhis.com>
2024-02-20 15:24:24 +0330

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,74 @@
 # ProxyIPResolver
+
+ProxyIPResolver is a Go application designed to accurately identify the real IP address of a client connecting to your server, even when requests are proxied through various layers like load balancers, CDNs (Content Delivery Networks), and reverse proxies. It extracts IP information from standard and non-standard HTTP headers and provides a comprehensive view of all request headers when needed.
+
+## Features
+
+- Extracts the client's real IP address from common and custom headers used by proxies and CDNs.
+- Supports a wide range of headers, including `X-Forwarded-For`, `X-Real-IP`, `CF-Connecting-IP` (Cloudflare), and more.
+- Option to display all HTTP request headers for debugging or informational purposes.
+- Simple, lightweight, and easy to integrate into existing Go web applications.
+
+## Getting Started
+
+### Prerequisites
+
+- Go (version 1.15 or later recommended)
+
+### Installation
+
+Clone the repository to your local machine:
+
+```bash
+git clone https://github.com/Babakkamali/ProxyIPResolver.git
+cd ProxyIPResolver
+```
+
+Build the application:
+ 
+```bash
+go build -o proxyipresolver
+```
+
+### Usage
+
+Run the application:
+
+```bash
+./proxyipresolver
+```
+
+By default, the application listens on port 3000 and can be accessed via `http://localhost:3000`. 
+
+To display all HTTP request headers, start the application with the `--show-all-headers` or `-all` flag:
+
+```bash
+./proxyipresolver --show-all-headers
+# or
+./proxyipresolver -all
+```
+
+### Docker Support
+
+You can also run ProxyIPResolver within a Docker container. Here's a simple `Dockerfile` you can use:
+
+
+Build and run the Docker container:
+
+```bash
+docker compose up -d
+```
+
+## Contributing
+
+Contributions are welcome! Feel free to open issues or submit pull requests with improvements or additional features.
+
+## License
+
+Distributed under the MIT License. See `LICENSE` for more information.
+
+## Contact
+
+Your Name - [info@babakkamali.com](mailto:info@babakkamali.com)
+
+Project Link: [https://github.com/Babakkamali/ProxyIPResolver](https://github.com/Babakkamali/ProxyIPResolver)
---
+++ main.go
@@ -0,0 +1,111 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"net"
+	"net/http"
+	"strings"
+)
+
+type headerCheck struct {
+    Name  string
+    CheckIP bool // true to check for real ip address
+}
+
+var headersToCheck = []headerCheck{
+    {"CF-Connecting-IP", true},				// Cloudflare CDN
+    {"True-Client-IP", true},				// Akamai and some others
+    {"Ar-Real-Ip", true},					// ArvanCloud CDN
+    {"X-Real-IP", true},
+    {"Forwarded-For", true},
+    {"X-Forwarded-For", true},
+    {"X-Client-Ip", false},
+    {"Forwarded", false},
+    {"X-Forwarded", false},		
+    {"X-Forwarded-Port", false},
+    {"X-Forwarded-Proto", false},
+    {"X-Forwarded-Server", false},
+    {"X-Forwarded-Host", false},
+    {"X-Country-Code", false},
+    {"Ar-Real-Country", false},				// ArvanCloud CDN
+    {"Upgrade-Insecure-Requests", false},
+    {"User-Agent", false},
+    {"Cdn-Loop", false},
+    {"Accept", false},
+    {"Accept-Encoding", false},
+}
+
+// getIP extracts the client's IP address from the request headers or RemoteAddr.
+func getIP(r *http.Request) string {
+	for _, header := range headersToCheck {
+		if header.CheckIP {
+			value := r.Header.Get(header.Name)
+			if value != "" {
+				ips := strings.Split(value, ",")
+				trimmedIp := strings.TrimSpace(ips[0])
+				if trimmedIp != "" {
+					return trimmedIp
+				}
+			}
+		}
+	}
+
+	ip, _, err := net.SplitHostPort(r.RemoteAddr)
+	if err != nil {
+		return r.RemoteAddr // Return the full remote address if splitting fails
+	}
+	return ip
+}
+
+func ipHandler(w http.ResponseWriter, r *http.Request, showAll bool) {
+	clientIp := getIP(r)
+	var response strings.Builder
+
+	// Display the client IP address at the beginning of the response
+	response.WriteString(fmt.Sprintf("\n\nYour IP address is %s\n\n\n\n", clientIp))
+
+	// Iterate through the predefined headers and format the output
+	for _, header := range headersToCheck {
+		values := r.Header.Values(header.Name)
+		if len(values) == 0 {
+		    // don't print for now
+		    //response.WriteString(fmt.Sprintf("%s: null\n\n", header.Name)) 
+		} else {
+			for _, value := range values {
+				response.WriteString(fmt.Sprintf("%s: %s\n\n", header.Name, value))
+			}
+		}
+	}
+	
+
+	// Include all headers if the flag is set
+	if showAll {
+		response.WriteString("\n\nAll Headers:\n")
+		for name, values := range r.Header {
+			for _, value := range values {
+				response.WriteString(fmt.Sprintf("%s: %s\n", name, value))
+			}
+		}
+	}
+
+	fmt.Fprint(w, response.String())
+}
+
+func main() {
+
+	showAllHeaders := flag.Bool("show-all-headers", false, "Show all HTTP headers")
+	flag.BoolVar(showAllHeaders, "all", false, "Show all HTTP headers (shorthand)")
+
+	flag.Parse()
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	ipHandler(w, r, *showAllHeaders)
+	})
+
+	fmt.Println("App listening on port 3000!")
+	if err := http.ListenAndServe("0.0.0.0:3000", nil); err != nil {
+		panic(err)
+	}
+}
+
