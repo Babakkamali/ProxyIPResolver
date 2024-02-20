e7dfd3cb438ecc02a68ac5a6f704211ee0979be8
Babak Kamali <b.kamali@yarhis.com>
2024-02-20 16:50:42 +0330

    [feat] add log middleware for main handlers

--- main.go
+++ main.go
@@ -6,34 +6,36 @@ import (
 	"net"
 	"net/http"
 	"strings"
+	"time"
+	"log"
 )
 
 type headerCheck struct {
-    Name  string
-    CheckIP bool // true to check for real ip address
+	Name    string
+	CheckIP bool // true to check for real ip address
 }
 
 var headersToCheck = []headerCheck{
-    {"CF-Connecting-IP", true},				// Cloudflare CDN
-    {"True-Client-IP", true},				// Akamai and some others
-    {"Ar-Real-Ip", true},					// ArvanCloud CDN
-    {"X-Real-IP", true},
-    {"X-Forwarded-For", true},
-    {"Forwarded-For", true},
-    {"X-Client-Ip", false},
-    {"Forwarded", false},
-    {"X-Forwarded", false},		
-    {"X-Forwarded-Port", false},
-    {"X-Forwarded-Proto", false},
-    {"X-Forwarded-Server", false},
-    {"X-Forwarded-Host", false},
-    {"X-Country-Code", false},
-    {"Ar-Real-Country", false},				// ArvanCloud CDN
-    {"Upgrade-Insecure-Requests", false},
-    {"User-Agent", false},
-    {"Cdn-Loop", false},
-    {"Accept", false},
-    {"Accept-Encoding", false},
+	{"CF-Connecting-IP", true}, // Cloudflare CDN
+	{"True-Client-IP", true},   // Akamai and some others
+	{"Ar-Real-Ip", true},       // ArvanCloud CDN
+	{"X-Real-IP", true},
+	{"X-Forwarded-For", true},
+	{"Forwarded-For", true},
+	{"X-Client-Ip", false},
+	{"Forwarded", false},
+	{"X-Forwarded", false},
+	{"X-Forwarded-Port", false},
+	{"X-Forwarded-Proto", false},
+	{"X-Forwarded-Server", false},
+	{"X-Forwarded-Host", false},
+	{"X-Country-Code", false},
+	{"Ar-Real-Country", false}, // ArvanCloud CDN
+	{"Upgrade-Insecure-Requests", false},
+	{"User-Agent", false},
+	{"Cdn-Loop", false},
+	{"Accept", false},
+	{"Accept-Encoding", false},
 }
 
 // getIP extracts the client's IP address from the request headers or RemoteAddr.
@@ -69,15 +71,14 @@ func ipHandler(w http.ResponseWriter, r *http.Request, showAll bool) {
 	for _, header := range headersToCheck {
 		values := r.Header.Values(header.Name)
 		if len(values) == 0 {
-		    // don't print for now
-		    //response.WriteString(fmt.Sprintf("%s: null\n\n", header.Name)) 
+			// don't print for now
+			//response.WriteString(fmt.Sprintf("%s: null\n\n", header.Name))
 		} else {
 			for _, value := range values {
 				response.WriteString(fmt.Sprintf("%s: %s\n\n", header.Name, value))
 			}
 		}
 	}
-	
 
 	// Include all headers if the flag is set
 	if showAll {
@@ -92,10 +93,19 @@ func ipHandler(w http.ResponseWriter, r *http.Request, showAll bool) {
 	fmt.Fprint(w, response.String())
 }
 
+// Logger is a middleware that logs the HTTP request details
+func Logger(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+		next.ServeHTTP(w, r)
+		log.Printf("%s %s %s %s", r.RemoteAddr, r.Method, r.RequestURI, time.Since(start))
+	}
+}
+
 func healthCheckHandler(w http.ResponseWriter, r *http.Request) {
-    // Respond with a simple message or JSON object indicating the service is up
-    w.WriteHeader(http.StatusOK) // 200 OK status
-    fmt.Fprintf(w, "O")
+	// Respond with a simple message or JSON object indicating the service is up
+	w.WriteHeader(http.StatusOK) // 200 OK status
+	fmt.Fprintf(w, "O")
 }
 
 func main() {
@@ -105,18 +115,18 @@ func main() {
 
 	flag.Parse()
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-	ipHandler(w, r, *showAllHeaders)
-	})
+    // Wrap the ipHandler with the Logger middleware
+    http.HandleFunc("/", Logger(func(w http.ResponseWriter, r *http.Request) {
+        ipHandler(w, r, *showAllHeaders)
+    }))
 
-	  // Handler for the /health endpoint
-	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
-        healthCheckHandler(w, r)
-    })
+	// Handler for the /health endpoint
+	http.HandleFunc("/health", Logger(func(w http.ResponseWriter, r *http.Request) {
+		healthCheckHandler(w, r)
+	}))
 
 	fmt.Println("App listening on port 3000!")
 	if err := http.ListenAndServe("0.0.0.0:3000", nil); err != nil {
 		panic(err)
 	}
 }
-
